a76ea41b24166c98677e583df6d991715cbeed3d
nicolas <jaljordan77@gmail.com>
2023-06-15 16:57:14 -0500

    update[calatlog->models/genre

--- catalog/models/genre.py
+++ catalog/models/genre.py
@@ -1,8 +1,10 @@
-
 from django.db import models
-
+from django.urls import reverse
 class Genre(models.Model):
     name = models.CharField(max_length=35)
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("genre_detail", args=[str(self.id)])
