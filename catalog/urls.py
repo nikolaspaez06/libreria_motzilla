8fc79974fa3304c42aa6851e743433cbf6dac282
nicolas <jaljordan77@gmail.com>
2023-06-15 16:58:51 -0500

    update [urls]

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,6 +4,8 @@ from .views import views
 from .views import author
 from .views import book_instance
 from .views import book
+from .views import genre
+from .views import language
 
 urlpatterns = [
     path ('',views.index,name= "index")
@@ -21,4 +23,23 @@ urlpatterns += [
     path("book",book.BookListView.as_view(),name='book'),
     path("book/<int:pk>",book.BookDetailtView.as_view(),name='book_detail'),
     path("book/create",book.BookCreateView.as_view(),name= 'create_book')
+]
+
+
+urlpatterns += [
+    path("book_instance",book_instance.BookInstanceListView.as_view(),name='book_instance'),
+    path("book_instance/<int:pk>",book_instance.BookInstanceDetailtView.as_view(),name='book_instance_detail'),
+    path("book_instance/create",book_instance.BookInstanceCreateView.as_view(),name= 'create-book_instance')
+]
+
+urlpatterns += [
+    path("genre",genre.GenreListView.as_view(),name='genre'),
+    path("genre/<int:pk>",genre.GenreDetailtView.as_view(),name='genre_detail'),
+    path("genre/create",genre.GenreCreateView.as_view(),name='create_genre')
+]   
+
+urlpatterns += [
+    path("language",language.LanguageListView.as_view(),name='language'),
+    path("language/<int:pk>",language.LanguageDetailtView.as_view(),name='language_detail'),
+    path("language/create",language.LanguageCreateView.as_view(),name= 'create_language')
 ]
