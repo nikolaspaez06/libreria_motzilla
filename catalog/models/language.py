0bfa3e7cb24383950a582b201acbc34c9ca72236
nicolas <jaljordan77@gmail.com>
2023-06-15 16:58:14 -0500

    update[catalog->model/lenguage]

--- catalog/models/language.py
+++ catalog/models/language.py
@@ -1,7 +1,10 @@
 from django.db import models
-
+from django.urls import reverse
 class Language(models.Model):
     name = models.CharField(max_length=35)
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("language_detail", args=[str(self.id)])
