45ffa958229ea06c6253d7a9d43c0cab2a7905fb
nicolas <jaljordan77@gmail.com>
2023-06-15 14:57:07 -0500

    update->modesl/book

--- catalog/models/book.py
+++ catalog/models/book.py
@@ -2,7 +2,7 @@ from django.db import models
 from .author import Author
 from .genre import Genre
 from .language import Language
-
+from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=60)
@@ -14,3 +14,7 @@ class Book(models.Model):
     
     def __str__(self) :
         return self.title 
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[str(self.id)])
+    
